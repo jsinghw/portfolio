1dec892c9bd53869bce1f65718bf2061dab3d43b
Jaspal Singh <jsinghw94@gmail.com>
2020-09-09 01:46:12 -0400

    form is functionality + validation complete

--- src/components/Portfolio/Contact/ContactForm.js
+++ src/components/Portfolio/Contact/ContactForm.js
@@ -1,7 +1,12 @@
 import React from "react";
+import {useRef} from 'react';
+import { useForm, Controller } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers';
+import * as yup from "yup";
 import emailjs from 'emailjs-com';
 import{ init } from 'emailjs-com';
 
+import { Input, TextField } from '@material-ui/core';
 
 import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
@@ -10,96 +15,96 @@ import Button from 'components/CustomButtons/Button.js';
 
 init("user_1ED4n9wgLPnUEAcEZymZd");
 
- export default class ContactForm extends React.Component {
-     state = {user_name: '',
-              user_email: '',
-              message: ''
-    };
+const schema = yup.object().shape({
+  email: yup.string().email("Enter a valid email.").required("Email is a required field."),
+  name: yup.string().max(100,).required("Name is a required field."),
+  message: yup.string().required("Message is a required field.")
+});
 
 
-    handleNameChange = event => {
-      this.setState({user_name: event.target.value});
-    }
-    handleEmailChange = event => {
-      this.setState({user_email: event.target.value});
-    }
-    handleMessageChange = event => {
-      this.setState({message: event.target.value});
-    }
+export default function ContactForm() {
+    let btnRef = useRef();
 
-    sendEmail = e => {
+    const sendEmail = (data, e) => {
       e.preventDefault();
+      if(btnRef.current){
+          btnRef.current.setAttribute("disabled", "disabled");
+        }
       emailjs.sendForm('jsinghw94@gmail.com',
                        'template_d8celzd',
                         e.target,
                         'user_1ED4n9wgLPnUEAcEZymZd',
-                        {
-                            user_name: this.state.user_name,
-                            user_email: this.state.user_email,
-                            message: this.state.message
-                        })
+                        data
+                        )
         .then((result) => {
             console.log(result.text);
+            if(btnRef.current){
+                btnRef.current.removeAttribute("disabled");
+            }
         }, (error) => {
             console.log(error.text);
+            if(btnRef.current){
+                btnRef.current.removeAttribute("disabled");
+            }
         });
     }
-    render() {
-        return (
-            <form onSubmit={this.sendEmail}>
-                <GridContainer
-                      direction="column"
-                      justify="center"
-                      alignItems="center"
-                      className="contact-grid">
-                      <GridItem xs={3} md={9}>
-                          <CustomInput
-                              labelText="Email"
-                              formControlProps={{
-                                  fullWidth: true
-                              }}
-                              inputProps={{onChange: this.handleEmailChange,
-                                           value: this.state.user_email,
-                                           name: "user_name",
-                                           required: true
-                                         }}
-                              />
-                      </GridItem>
-                      <GridItem xs={3} md={9}>
-                          <CustomInput
-                              labelText="Name"
-                              formControlProps={{
-                                  fullWidth: true
-                              }}
-                              inputProps={{onChange: this.handleNameChange,
-                                           value: this.state.user_name,
-                                           name: "user_name",
-                                           required: true
-                                         }}
-                              />
-                      </GridItem>
-                      <GridItem xs={3} md={9}>
-                          <CustomInput
-                              labelText="Message"
-                              inputProps={{multiline: true,
-                                           rows: 10,
-                                           onChange: this.handleMessageChange,
-                                           value: this.state.message,
-                                           required: true,
-                                           name: "message"
-                                         }}
-                              formControlProps={{
-                                  fullWidth: true
-                              }}
-                              />
-                      </GridItem>
-                      <GridItem xs={3} md={3}>
-                          <div className="form-button">
-                              <Button type="submit">Submit</Button>
-                          </div>
-                      </GridItem>
-                </GridContainer>
-            </form>
-        )
-    }
+
+    const { handleSubmit, register, errors } = useForm({
+        resolver: yupResolver(schema),
+        mode: 'onChange'
+    });
+
+    return (
+        <form onSubmit={handleSubmit(sendEmail)}>
+            <GridContainer
+                  direction="column"
+                  justify="center"
+                  alignItems="center"
+                  className="contact-grid">
+                  <GridItem xs={3} md={9}>
+                      <TextField
+                          className="form-input"
+                          name="email"
+                          label="Email"
+                          required
+                          fullWidth="true"
+                          error={errors.email? true:false}
+                          inputRef={register({required: true})}
+                          helperText={errors.email? errors.email.message:"\u00a0"}
+                      />
+                  </GridItem>
+                  <GridItem xs={3} md={9}>
+                      <TextField
+                          className="form-input"
+                          name="name"
+                          label="Name"
+                          required
+                          fullWidth="true"
+                          error={errors.name? true:false}
+                          inputRef={register({required: true})}
+                          helperText={errors.name? errors.name.message:"\u00a0"}
+                      />
+                  </GridItem>
+                  <GridItem xs={3} md={9}>
+                      <TextField
+                          className="form-input"
+                          name="message"
+                          label="Message"
+                          required
+                          fullWidth="true"
+                          multiline="true"
+                          rows="10"
+                          error={errors.message? true:false}
+                          inputRef={register({required: true})}
+                          helperText={errors.message? errors.message.message:"\u00a0"}
+                          />
+                  </GridItem>
+                  <GridItem xs={3} md={3}>
+                      <div className="form-button">
+                          <Button type="submit" ref={btnRef}>Submit</Button>
+                      </div>
+                  </GridItem>
+            </GridContainer>
+        </form>
+    )
 }
