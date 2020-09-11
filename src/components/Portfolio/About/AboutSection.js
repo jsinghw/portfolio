a64e16a554af51db6afed5b2017c33b759ddc68a
Jaspal Singh <jsinghw94@gmail.com>
2020-09-11 14:35:41 -0400

    minor style changes to about section

--- src/components/Portfolio/About/AboutSection.js
+++ src/components/Portfolio/About/AboutSection.js
@@ -2,15 +2,19 @@ import React from "react";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
 // core components
+import Paper from '@material-ui/core/Paper';
 import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
 import Card from "components/Card/Card.js";
 import CardHeader from "components/Card/CardHeader.js";
 import CardBody from "components/Card/CardBody.js";
 import CardFooter from "components/Card/CardFooter.js";
-import CardAvatar from "components/Card/CardAvatar.js";
 import Button from "components/CustomButtons/Button.js";
 import Info from "components/Typography/Info.js";
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
 import cardProfile4 from "assets/img/examples/card-profile4.jpg";
@@ -19,6 +23,11 @@ import cardProfile4 from "assets/img/examples/card-profile4.jpg";
 import styles from "assets/jss/material-kit-pro-react/views/componentsSections/sectionCards.js";
 
 const useStyles = makeStyles(styles);
+
+
+library.add(fab)
+
+
 
 export default function AboutSection() {
   const classes = useStyles();
@@ -108,23 +117,20 @@ export default function AboutSection() {
                               profile
                               className={classes.justifyContentCenter}
                               >
-                              <Button justIcon simple color="twitter">
-                                  <i className="fab fa-twitter" />
+                              <Button color="linkedin" href="https://www.linkedin.com/in/jaspalsingh94/" justIcon>
+                                  <FontAwesomeIcon icon={['fab', 'linkedin']} className="svg-inline--fa.fa-w-14" />
                               </Button>
-                              <Button justIcon simple color="dribbble">
-                                  <i className="fab fa-dribbble" />
-                              </Button>
-                              <Button justIcon simple color="instagram">
-                                  <i className="fab fa-instagram" />
+                              <Button color="github" href="https://github.com/jsinghw" justIcon>
+                                  <FontAwesomeIcon icon={['fab', 'github']} className="svg-inline--fa.fa-w-16" />
                               </Button>
                           </CardFooter>
                       </Card>
                   </GridItem>
                   <GridItem xs={12} sm={4} md={8} className='about-text-wrapper'>
-                      <div className='about-text'>
+                      <Paper className='about-text'>
                           Change md to 4 if you want to switch back to 2/3 of the screen.
                           I'm baby edison bulb taxidermy banh mi squid tumblr raw denim. Taiyaki jianbing etsy street art typewriter succulents authentic. Everyday carry four dollar toast fam, man bun gluten-free tumblr banh mi kogi palo santo artisan. Mlkshk yuccie subway tile hell of jean shorts cold-pressed artisan drinking vinegar woke organic mustache scenester. Artisan +1 synth salvia. Roof party shabby chic DIY, butcher four loko portland twee williamsburg vegan knausgaard ramps echo park. Hell of vice la croix, slow-carb flexitarian kogi leggings prism kitsch.
-                      </div>
+                      </Paper>
                   </GridItem>
               </GridContainer>
           </div>
