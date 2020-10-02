4d8a1397df5db63161fae45796291ce3c76409dc
Jaspal Singh <jsinghw94@gmail.com>
2020-10-02 18:19:25 -0400

    Updates

---
+++ precache-manifest.ef0b40373c269cebaef67b7043bdc95c.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82ba04d205db068c0ea9d2d69b66c714",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "03e10c7169243c3c9c60",
+    "url": "/portfolio/static/css/main.92e6ed53.chunk.css"
+  },
+  {
+    "revision": "dbfd864a4aaf0d778382",
+    "url": "/portfolio/static/js/2.0b71f3cc.chunk.js"
+  },
+  {
+    "revision": "75304a605f9c03e664948edc1c5186bb",
+    "url": "/portfolio/static/js/2.0b71f3cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03e10c7169243c3c9c60",
+    "url": "/portfolio/static/js/main.5deb0c77.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "a06651da552962ac812593626d58481a",
+    "url": "/portfolio/static/media/profile.a06651da.jpg"
+  },
+  {
+    "revision": "f95789b9569098d45f915ca9a6a90483",
+    "url": "/portfolio/static/media/proj1-1.f95789b9.png"
+  },
+  {
+    "revision": "f66718e846e05e60525afede19ade63c",
+    "url": "/portfolio/static/media/proj1-2.f66718e8.png"
+  },
+  {
+    "revision": "7305c7a3173fef55921f4479e64f2884",
+    "url": "/portfolio/static/media/proj1-3.7305c7a3.png"
+  },
+  {
+    "revision": "054566953af68d59b399763ba91f0684",
+    "url": "/portfolio/static/media/proj1-4.05456695.png"
+  },
+  {
+    "revision": "99ebbc8f8bcb6ac1eac5a842796e94aa",
+    "url": "/portfolio/static/media/proj1-5.99ebbc8f.png"
+  },
+  {
+    "revision": "679e573adff7713c76d2ed7af888829b",
+    "url": "/portfolio/static/media/proj1-6.679e573a.png"
+  },
+  {
+    "revision": "6078bab3cd0564b986072f35e84c9ba6",
+    "url": "/portfolio/static/media/proj1-7.6078bab3.png"
+  },
+  {
+    "revision": "8fcdac93013c9e0f0e6685baeb9f51f9",
+    "url": "/portfolio/static/media/proj1-8.8fcdac93.png"
+  },
+  {
+    "revision": "9bd2ab4af2628dcec468ece2473283ae",
+    "url": "/portfolio/static/media/proj2-1.9bd2ab4a.png"
+  },
+  {
+    "revision": "67be8988eb4569655f33e8265562fe49",
+    "url": "/portfolio/static/media/proj2-2.67be8988.png"
+  },
+  {
+    "revision": "ee22568e25fbc69810a0754fbac3316a",
+    "url": "/portfolio/static/media/proj2-3.ee22568e.png"
+  },
+  {
+    "revision": "346eb49711389c672457a0d49c08229d",
+    "url": "/portfolio/static/media/proj2-4.346eb497.png"
+  },
+  {
+    "revision": "3b901f0a1841f619134160f68744d6ca",
+    "url": "/portfolio/static/media/proj2-5.3b901f0a.png"
+  },
+  {
+    "revision": "e826c2026dda1cb0aecbbff5f1d1f07d",
+    "url": "/portfolio/static/media/proj2-6.e826c202.png"
+  },
+  {
+    "revision": "b239cc2e4770fc483941e216b71c23e6",
+    "url": "/portfolio/static/media/proj3-1.b239cc2e.png"
+  },
+  {
+    "revision": "b721b880101b7136adc48530cd429c48",
+    "url": "/portfolio/static/media/proj3-2.b721b880.png"
+  },
+  {
+    "revision": "69775c44888bc006486196793df7fdb1",
+    "url": "/portfolio/static/media/proj3-3.69775c44.png"
+  },
+  {
+    "revision": "d9edda94f91f4bf9432363f30842368c",
+    "url": "/portfolio/static/media/proj3-4.d9edda94.png"
+  },
+  {
+    "revision": "bc07d35ea7c62656875e07efbee37f41",
+    "url": "/portfolio/static/media/proj4-1.bc07d35e.png"
+  },
+  {
+    "revision": "a9ffad25457760e32bfba97dbd7714e3",
+    "url": "/portfolio/static/media/proj4-2.a9ffad25.png"
+  },
+  {
+    "revision": "ced911b1812f7bd683a7dc5f258c0352",
+    "url": "/portfolio/static/media/proj4-3.ced911b1.png"
+  },
+  {
+    "revision": "7c5c202460ed45baa1cd14c079b637b0",
+    "url": "/portfolio/static/media/proj4-4.7c5c2024.png"
+  },
+  {
+    "revision": "837454d8a4bd628d179e1006c6c3ac79",
+    "url": "/portfolio/static/media/proj4-5.837454d8.png"
+  },
+  {
+    "revision": "6cd18bc0983f441a1b55d38408a271a7",
+    "url": "/portfolio/static/media/wave-bot.6cd18bc0.png"
+  },
+  {
+    "revision": "ba75e0c9c558e20911df32aa3cc7a5bd",
+    "url": "/portfolio/static/media/wave-mid.ba75e0c9.png"
+  },
+  {
+    "revision": "05d52988a72cb1da374bc1690f060224",
+    "url": "/portfolio/static/media/wave-top.05d52988.png"
+  }
+]);
